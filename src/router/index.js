92ab17c8ccda1b74d9f463bd2b5e2f916f2ac76f
Jeremiah Wodke <jeremiah.wodke@gmail.com>
2020-10-16 07:41:12 -0600

    adding title and meta tags to portfolio

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Contact from "@/views/Contact";
 import Resume from "@/views/Resume";
 import Projects from "@/views/Projects";
 import ProjectSingle from "@/views/ProjectSingle";
+import titleMetaTagsGuard from "@/router/guards/title-metatags.guard";
 
 Vue.use(VueRouter);
 
@@ -13,27 +14,49 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
+    meta: {
+      title: "HOME - Jeremiah Wodke Full-Stack Web Developer",
+      description:
+        "I have a passion for building full-stack JavaScript applications to improve the lives of my clients and their customers alike."
+    }
   },
   {
     path: "/about",
     name: "About",
-    component: About
+    component: About,
+    meta: {
+      title: "ABOUT - Jeremiah Wodke Full-Stack Web Developer",
+      description:
+        "Enthusiastic full-stack developer with a passion for continuous growth, and innovation..."
+    }
   },
   {
     path: "/projects",
     name: "Projects",
-    component: Projects
+    component: Projects,
+    meta: {
+      title: "PROJECTS - Jeremiah Wodke Full-Stack Web Developer",
+      description:
+        "Portfolio of projects where Jeremiah was the sole developer or lead of given website project or application..."
+    }
   },
   {
     path: "/projects/:id",
     name: "Project",
-    component: ProjectSingle
+    component: ProjectSingle,
+    meta: {
+      title: "PROJECT - Jeremiah Wodke Full-Stack Web Developer"
+    }
   },
   {
     path: "/contact",
     name: "Contact",
-    component: Contact
+    component: Contact,
+    meta: {
+      title: "CONTACT - Jeremiah Wodke Full-Stack Web Developer",
+      description: "Interested in working with me? Shoot me a message!"
+    }
   },
   {
     path: "/resume",
@@ -45,5 +68,7 @@ const routes = [
 const router = new VueRouter({
   routes
 });
+
+router.beforeEach(titleMetaTagsGuard);
 
 export default router;
