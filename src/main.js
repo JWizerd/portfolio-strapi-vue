a5961cd27e448ef183b5e7089bfddb8273d6b553
Jeremiah Wodke <jeremiah.wodke@gmail.com>
2021-11-02 15:34:40 -0600

    remove pixabay service from bootstrap

--- src/main.js
+++ src/main.js
@@ -4,13 +4,11 @@ import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
 import Strapi from "@autom8apps/strapi-sdk-javascript";
-import Pixabay from "@/services/pixabay";
 
 Vue.config.productionTip = false;
 store.$env = process.env.NODE_ENV;
 store.$username = process.env.VUE_APP_BACKEND_USERNAME;
 store.$token = process.env.VUE_APP_BACKEND_PUBLIC_TOKEN;
-store.$pixabay = new Pixabay(process.env.VUE_APP_PIXABAY_KEY);
 store.$strapi = new Strapi(process.env.VUE_APP_BACKEND_PRODUCTION_URL);
 
 new Vue({
